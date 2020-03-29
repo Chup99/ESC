85b460febfa81c282053f518852a3e3b03df72a8
AnAsianGangster <AnAsianGangster@users.noreply.github.com>
2020-03-30 00:05:50 +0800

    folder unreachable fix

---
+++ dataVisual copy/src/app/queue/queue.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+// import {  } from "../customer/customer.component.spec";
+
+@Component({
+  selector: 'app-queue',
+  templateUrl: './queue.component.html',
+  styleUrls: ['./queue.component.css']
+})
+export class QueueComponent implements OnInit {
+
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
