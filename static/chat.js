2e4a7ffa4a6f1ffa5c5245cdc0deb12aa74b8b00
Hadi_Alwi <hadi_alwi@mymail.sutd.edu.sg>
2020-04-07 21:20:23 +0800

    Trying to connect to rainbow

--- static/chat.js
+++ static/chat.js
@@ -1,30 +1,19 @@
-// function openForm() {
-//   document.getElementById("myForm").style.display = "block";
-// }
-
-// function closeForm() {
-//   document.getElementById("myForm").style.display = "none";
-// }
-
+initialize();
 const msgerForm = document.getElementById("inputArea");
-console.log(msgerForm);
 const msgerInput = document.getElementById("input");
 const msgerChat = document.getElementById("chat");
 console.log("things are loaded");
 
-const BOT_MSGS = [
-  "Hi, how are you?",
-  "Ohh... I can't understand what you trying to say. Sorry!",
-  "I like to play games... But I don't know how to play!",
-  "Sorry if my answers are not relevant. :))",
-  "I feel sleepy! :("
-];
-
 // Icons made by Freepik from www.flaticon.com
-const BOT_IMG = "https://image.flaticon.com/icons/svg/327/327779.svg";
+
+const BOT_IMG = "https://image.flaticon.com/icons/svg/145/145867.svg";
 const PERSON_IMG = "https://image.flaticon.com/icons/svg/145/145867.svg";
-const BOT_NAME = "BOT";
-const PERSON_NAME = "Sajad";
+const BOT_NAME = "Agent";
+const PERSON_NAME = "user";
+var AGENT_ACC = rainbowSDK.contacts.searchById("5e60603ad8084c29e64eb5f5");
+console.log(AGENT_ACC);
+var CONVO = rainbow.conversations.openConversationForContact(AGENT_ACC);
+console.log("Agent connected");
 
 msgerForm.addEventListener("submit", event => {
   event.preventDefault();
@@ -32,11 +21,25 @@ msgerForm.addEventListener("submit", event => {
   const msgText = msgerInput.value;
   if (!msgText) return;
 
+  rainbowSDK.im.sendMessageToConversation(CONVO, msgText);
+
   appendMessage(PERSON_NAME, PERSON_IMG, "right", msgText);
   msgerInput.value = "";
-
-  botResponse();
 });
+
+let onNewMessageReceived = function(event) {
+  let message = event.detail.message;
+  let conversation = event.detail.conversation;
+
+  appendMessage(BOT_NAME, BOT_IMG, "left", message);
+
+  // Do something with the new message received
+};
+
+document.addEventListener(
+  rainbowSDK.im.RAINBOW_ONNEWIMMESSAGERECEIVED,
+  onNewMessageReceived
+);
 
 function appendMessage(name, img, side, text) {
   //   Simple solution for small apps
@@ -59,29 +62,11 @@ function appendMessage(name, img, side, text) {
   msgerChat.scrollTop += 500;
 }
 
-function botResponse() {
-  const r = random(0, BOT_MSGS.length - 1);
-  const msgText = BOT_MSGS[r];
-  const delay = msgText.split(" ").length * 100;
-
-  setTimeout(() => {
-    appendMessage(BOT_NAME, BOT_IMG, "left", msgText);
-  }, delay);
-}
-
 // Utils
-function get(selector, root = document) {
-  console.log(root.querySelector(selector));
-  return root.querySelector(selector);
-}
 
 function formatDate(date) {
   const h = "0" + date.getHours();
   const m = "0" + date.getMinutes();
 
   return `${h.slice(-2)}:${m.slice(-2)}`;
-}
-
-function random(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
 }
